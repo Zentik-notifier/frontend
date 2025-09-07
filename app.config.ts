801700f308259b331abb8a89de297bb862740512
Gianluca Ruocco <gianluca.ruocco@xarvio.com>
2025-09-07 22:59:44 +0200

    Version updated

--- app.config.ts
+++ app.config.ts
@@ -23,7 +23,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name,
         slug: "zentik",
-        version: "1.1.5",
+        version: "1.1.6",
         orientation: "portrait",
         icon: "./assets/icons/icon-512x512.png",
         scheme: "zentik",
@@ -39,7 +39,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
             supportsTablet: true,
             bundleIdentifier,
             appleTeamId: "C3F24V5NS5",
-            buildNumber: "12",
+            buildNumber: "13",
             icon: "./assets/icons/icon-512x512.png",
             googleServicesFile: process.env.GOOGLE_SERVICE_INFO_PLIST ?? './keys/GoogleService-Info.plist',
             infoPlist: {
@@ -63,7 +63,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
                 backgroundColor: "#ffffff",
             },
             package: bundleIdentifier,
-            versionCode: 12,
+            versionCode: 13,
             googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './keys/google-services.json',
             permissions: [
                 "android.permission.RECEIVE_BOOT_COMPLETED",
