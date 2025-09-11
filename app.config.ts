ce1989838e24721bb6de7bc08e1d32c4e9814079
Gianluca Ruocco <gianluca.ruocco@xarvio.com>
2025-09-11 09:49:55 +0200

    chore(ios): bump version to 1.1.11 (build 18)

--- app.config.ts
+++ app.config.ts
@@ -23,7 +23,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name,
         slug: "zentik",
-        version: "1.1.10",
+        version: "1.1.11",
         orientation: "portrait",
         icon: "./assets/icons/icon-512x512.png",
         scheme: "zentik",
@@ -39,7 +39,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
             supportsTablet: true,
             bundleIdentifier,
             appleTeamId: "C3F24V5NS5",
-            buildNumber: "17",
+            buildNumber: "18",
             icon: "./assets/icons/icon-512x512.png",
             googleServicesFile: process.env.GOOGLE_SERVICE_INFO_PLIST ?? './keys/GoogleService-Info.plist',
             infoPlist: {
@@ -63,7 +63,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
                 backgroundColor: "#ffffff",
             },
             package: bundleIdentifier,
-            versionCode: 17,
+            versionCode: 18,
             googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './keys/google-services.json',
             permissions: [
                 "android.permission.RECEIVE_BOOT_COMPLETED",
