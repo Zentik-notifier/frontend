59a0a499fa195a7ccc14f7bcf32005c8c69d9cfa
Gianluca Ruocco <gianluca.ruocco@xarvio.com>
2025-10-19 00:23:11 +0200

    chore(ios): bump version to 1.4.13 (build 46)

--- app.config.ts
+++ app.config.ts
@@ -23,7 +23,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name,
         slug: "zentik",
-        version: "1.4.12",
+        version: "1.4.13",
         orientation: "default",
         icon: "./assets/icons/generators/glas_default.png",
         scheme: "zentik",
@@ -43,7 +43,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
             supportsTablet: true,
             bundleIdentifier,
             appleTeamId: "C3F24V5NS5",
-            buildNumber: "45",
+            buildNumber: "46",
             icon: "./assets/icons/generators/glas_default.png",
             // icon: {
             //     light: "./assets/icons/generators/glas_default.png",
@@ -73,7 +73,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
                 backgroundColor: "#ffffff",
             },
             package: bundleIdentifier,
-            versionCode: 45,
+            versionCode: 46,
             googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './keys/google-services.json',
             permissions: [
                 "android.permission.RECEIVE_BOOT_COMPLETED",
