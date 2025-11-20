aacb40b05d871079162df2453e68262d35ba926c
Gianluca Ruocco <gianluca.ruocco@xarvio.com>
2025-11-20 14:35:55 +0100

    chore(ios): bump version to 1.6.21 (build 75)

--- app.config.ts
+++ app.config.ts
@@ -32,7 +32,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name,
         slug: "zentik",
-        version: "1.6.20",
+        version: "1.6.21",
         orientation: "default",
         icon: "./assets/icons/generators/glas_default.png",
         scheme,
@@ -60,7 +60,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
             supportsTablet: true,
             bundleIdentifier,
             appleTeamId: "C3F24V5NS5",
-            buildNumber: "74",
+            buildNumber: "75",
             usesAppleSignIn: true,
             associatedDomains: [
                 "applinks:notifier.zentik.app"
@@ -94,7 +94,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
                 backgroundColor: "#ffffff",
             },
             package: bundleIdentifier,
-            versionCode: 74,
+            versionCode: 75,
             googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './keys/google-services.json',
             permissions: [
                 "android.permission.RECEIVE_BOOT_COMPLETED",
