29b2caf2c4ef0ccc1583cd6186516e0645a2b13d
Gianluca Ruocco <gianluca.ruocco@xarvio.com>
2026-02-11 09:55:17 +0100

    chore(ios): bump version to 1.7.26 (build 112)

--- app.config.ts
+++ app.config.ts
@@ -38,7 +38,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name,
         slug: "zentik",
-        version: "1.7.25",
+        version: "1.7.26",
         orientation: "default",
         icon: "./assets/icons/generators/glas_default.png",
         scheme,
@@ -84,7 +84,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
             supportsTablet: true,
             bundleIdentifier,
             appleTeamId: "C3F24V5NS5",
-            buildNumber: "111",
+            buildNumber: "112",
             usesAppleSignIn: true,
             associatedDomains: [
                 "applinks:notifier.zentik.app"
@@ -117,7 +117,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
                 backgroundColor: "#ffffff",
             },
             package: bundleIdentifier,
-            versionCode: 111,
+            versionCode: 112,
             permissions: [
                 "android.permission.RECEIVE_BOOT_COMPLETED",
                 "android.permission.WAKE_LOCK",
