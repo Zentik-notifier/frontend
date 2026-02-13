b54dab513e321ace936aace875518eefd3dfca11
Gianluca Ruocco <gianluca.ruocco@xarvio.com>
2026-02-13 12:28:13 +0100

    chore(ios): bump version to 1.8.3 (build 119)

--- app.config.ts
+++ app.config.ts
@@ -16,7 +16,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name,
         slug: "zentik",
-        version: "1.8.2",
+        version: "1.8.3",
         orientation: "default",
         icon: "./assets/icons/generators/glas_default.png",
         scheme,
@@ -62,7 +62,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
             supportsTablet: true,
             bundleIdentifier,
             appleTeamId: "C3F24V5NS5",
-            buildNumber: "118",
+            buildNumber: "119",
             usesAppleSignIn: true,
             associatedDomains: [
                 "applinks:notifier.zentik.app"
@@ -96,7 +96,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => {
                 backgroundColor: "#ffffff",
             },
             package: bundleIdentifier,
-            versionCode: 118,
+            versionCode: 119,
             permissions: [
                 "android.permission.RECEIVE_BOOT_COMPLETED",
                 "android.permission.WAKE_LOCK",
